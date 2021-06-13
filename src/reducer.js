f6662617b9879bf3ed370d58ca5b736fba3058fd
Julián Mutchinick <julimuchi@gmail.com>
2021-06-13 12:32:48 -0300

    First commit

---
+++ src/reducer.js
@@ -0,0 +1,77 @@
+import CartItem from "./CartItem"
+
+const reducer = (state, action)=>{
+    if(action.type === 'CLEAR_CART'){
+        return {...state, cart:[]}
+    }
+    if (action.type === 'REMOVE') {
+        let newItems = state.cart.filter((cartItem)=>cartItem.id != action.playload)
+        return {
+            ...state,
+            cart: newItems
+        }
+    }
+    if (action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem)=>{
+            if(cartItem.id === action.playload){
+                return {...cartItem, amount:cartItem.amount + 1}
+            }
+            return cartItem
+        })
+        return {
+            ...state,
+            cart: tempCart
+        }
+    }
+    
+    if (action.type === 'DECREASE') {
+        let tempCart = state.cart.map((cartItem)=>{
+            if(cartItem.id === action.playload){
+                return {...cartItem, amount:cartItem.amount - 1}
+            }
+            return cartItem
+        }).filter((cartItem)=>cartItem.amount !== 0)
+        return {
+            ...state,
+            cart: tempCart
+        }
+    }
+
+    if (action.type === 'GET_TOTALS') {
+        console.log("hello")
+        let {total, amount} = state.cart.reduce((cartTotal, cartItem)=>{
+            const {price, amount} = cartItem;
+            const itemTotal = price * amount;
+
+            cartTotal.total += itemTotal;
+            cartTotal.amount += amount;
+            return cartTotal;
+        },{
+            total:0,
+            amount:0
+        })
+
+        total = parseFloat(total.toFixed(2))
+
+        return {...state, total, amount}
+    }
+
+    if (action.type === 'LOADING') {
+        return {
+            ...state,
+            loading: true
+        }
+    }
+    
+    if (action.type === 'DISPLAY_ITEMS') {
+        return {
+            ...state,
+            cart: action.playload,
+            loading:false
+        }
+    }
+
+    return state
+}
+
+export default reducer
